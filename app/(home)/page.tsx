3deb4ac16329eac8218fd3578e2640d7235300ea
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2024-02-25 11:34:00 +0100

    remove most of the remaining stuff I wasn't going to use

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import { useCursors } from "./cursors-provider";
 
 export default function Home() {
@@ -16,23 +15,8 @@ export default function Home() {
       </section>
 
       <section className="flex flex-col gap-2">
-        <h1 className="text-4xl font-medium pb-6">PartyKit Starter Kit</h1>
-        <p>What you’ll find here...</p>
-        <ul className="list-disc list-inside">
-          <li>Multiplayer chatrooms</li>
-          <li>AI chatbots</li>
-          <li>Sample client and party code for all of the above</li>
-        </ul>
-        <p>
-          Check <code>README.md</code> for how to run this locally in 3 steps.
-        </p>
+        <h1 className="text-4xl font-medium pb-6">PartyKit Game of Set</h1>
       </section>
-
-      <Link href="/chat" className="underline">
-        <button className="flex items-center justify-center px-10 py-6 border border-stone-200 rounded-lg shadow hover:shadow-md">
-          AI Chat -&gt;
-        </button>
-      </Link>
     </div>
   );
 }
