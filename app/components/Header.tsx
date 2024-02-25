350ec3ed2f6c169491bc02c40b27a2099b408a65
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2024-02-25 11:50:46 +0100

    fix style overlap

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,7 +4,7 @@ import Avatar from "./Avatar";
 
 export default async function Header() {
   return (
-    <header className="z-10 p-4 sm:p-6 w-full border-b border-stone-300 absolute sticky top-0 bg-white/80 backdrop-blur">
+    <header className="z-10 p-4 sm:p-6 w-full border-b border-stone-300 sticky top-0 bg-white/80 backdrop-blur">
       <nav className="max-w-7xl m-auto flex justify-between items-center">
         <Link href="/">
           <h1 className="font-medium my-2">🎈Starter Kit</h1>
