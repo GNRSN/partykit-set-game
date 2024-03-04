c3d8d18c13e7e120a2bc0a017085a8002fd25fa1
Erik Gunnarsson <erik.gunnarsson@yolean.com>
2024-03-04 21:46:30 +0100

    configure eslint with flat config

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+import globals from "globals";
+import nextPlugin from "@next/eslint-plugin-next";
+import reactPlugin from "eslint-plugin-react";
+import hooksPlugin from "eslint-plugin-react-hooks";
+
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      semi: "warn",
+      "no-trailing-spaces": "warn",
+      "comma-dangle": ["warn", "always-multiline"],
+    },
+  },
+  {
+    plugins: {
+      react: reactPlugin,
+    },
+    rules: {
+      ...reactPlugin.configs["jsx-runtime"].rules,
+    },
+    settings: {
+      react: {
+        version: "detect", // You can add this if you get a warning about the React version when you lint
+      },
+    },
+  },
+  {
+    plugins: {
+      "react-hooks": hooksPlugin,
+    },
+    rules: hooksPlugin.configs.recommended.rules,
+  },
+  {
+    plugins: {
+      "@next/next": nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+    },
+  },
+  {
+    ignores: [".next/*", ".partykit/*"],
+  },
+];
