d5d2470d57b85ca6988cae5bd2f54974f928a422
GNRSN <9020004+GNRSN@users.noreply.github.com>
2024-03-17 16:28:20 +0100

    parse env with t3-oss

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const { fileURLToPath } = require("node:url");
+const createJiti = require("jiti");
+
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
+// Import env here to validate during build. Using jiti we can import .ts files :)
+jiti("./app/env");
+
+// REVIEW: Remove this
 const PARTYKIT_HOST = process.env.NEXT_PUBLIC_PARTYKIT_HOST || "127.0.0.1:1999";
 const PARTYKIT_PROTOCOL =
   PARTYKIT_HOST?.startsWith("localhost") ||
